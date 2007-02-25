44ff196ab3544d71ce1e1488fc123256445503e0
Miroslav Šulc <fordfrog@gentoo.org>
2007-02-25 14:28:13 +0000

    Milestone 7 ebuild
    svn path=/migrated-java-experimental-overlay/; revision=3894

--- dev-util/netbeans/files/6.0/startscript.sh
+++ dev-util/netbeans/files/6.0/startscript.sh
@@ -1,21 +1,2 @@
 #!/bin/bash
-
-check_symlinks() {
-	for file in `find ${1} -type l` ; do
-
-		if ! test -e ${file} ; then
-			echo "Broken symlink:"
-			echo "${file}"
-		fi
-
-	done
-}
-
-NBDIR="/usr/share/netbeans-6.0"
-
-check_symlinks ${NBDIR}/ide8/modules/ext
-check_symlinks ${NBDIR}/ide8/modules/autoload/ext
-check_symlinks ${NBDIR}/enterprise4/config/TagLibraries/JSTL11
-check_symlinks ${NBDIR}/platform7/modules/ext
-
-${NBDIR}/bin/netbeans "$@"
+/usr/share/netbeans-6.0/bin/netbeans "$@"
