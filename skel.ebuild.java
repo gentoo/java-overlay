0567c6b4c3feb62b5b3b63fbc8da8ee2b87d2928
Andreas Sturmlechner <andreas.sturmlechner@gmail.com>
2015-08-09 02:34:45 +0200

    Update skel.ebuild.java
    Package-Manager: portage-2.2.20

--- skel.ebuild.java
+++ skel.ebuild.java
@@ -1,6 +1,6 @@
-# Copyright 1999-2008 Gentoo Foundation
+# Copyright 1999-2015 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: $
+# $Id$
 
 EAPI=1
 JAVA_PKG_IUSE="doc source"
